3036985981e898ae9ee96d743ac8064bfbab28e4
angelmarybinu <98399507+angelmarybinu@users.noreply.github.com>
2023-05-12 08:40:27 +0530

    commit

--- script.js
+++ script.js
@@ -3,5 +3,35 @@
 
 const dictionary = (word) => {
 
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'b0b253ac35msh55edd6240dbcf01p1521e1jsn2d4be0fbb17f',
+            'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        
+        
+        
+        console.log(result);
+        
+        word.innerHTML=response.word;
+        definition.innerHTML=response.definition;
+
+
+
+    } catch (error) {
+        console.error(error);
+    }
     
 }
+
+submitbtn.addEventListener("click",(e)=>
+{
+    e.preventDefault();
+    dictionary(searchinput.value)
+})
